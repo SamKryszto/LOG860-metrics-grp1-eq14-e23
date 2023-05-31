5affec26dc3e42a443b7119a7249166ce9246032
Fabrice Lu <fabrice.lu.1@ens.etsmtl.ca>
2023-05-30 23:24:05 -0400

    localhost:3000 affiche swaggerUI

--- app/src/index.js
+++ app/src/index.js
@@ -5,13 +5,13 @@ const metricRouter = require("./routes/metric");
 const swaggerUI = require('swagger-ui-express');
 const yaml = require('yaml');
 const fs = require('fs');
-const doc = fs.readFileSync('./swagger/api.yaml');
+const doc = fs.readFileSync('./src/swagger/api.yaml','utf8');
 const api = yaml.parse(doc);
 
 
 
 
-//app.use("/", swaggerUI.serve,swaggerUI.setup(api));
+app.use("/", swaggerUI.serve,swaggerUI.setup(api));
 
 app.use("/metric", metricRouter);
 
