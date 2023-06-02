997ea01b52476f3f82d38a16e51b527c5d6a8beb
michel.sialer-cruz.1@ens.etsmtl.ca <michel.sialer-cruz.1@ens.etsmtl.ca>
2023-06-01 23:05:31 -0400

    unit tests for pr metrics

--- app/test/services/github.test.js
+++ app/test/services/github.test.js
@@ -3,24 +3,30 @@ const githubService = require("../../src/services/github");
 
 jest.mock("axios");
 
+const createRes = (repository) => {
+    return {
+        data: {
+            data: {
+                repository,
+            },
+        },
+    };
+};
+
+// ---------------
 // async function getTaskLeadTime(repo, owner, id)
+// ---------------
 describe("getTaskLeadTime", () => {
     const id = "1";
 
     describe("when graphql call returns an issue with createdAt and closedAt property", () => {
         it("should return correct calculated lead time", async () => {
-            const res = {
-                data: {
-                    data: {
-                        repository: {
-                            issue: {
-                                createdAt: "2023-05-26T15:53:06Z",
-                                closedAt: "2023-05-26T16:00:37Z",
-                            },
-                        },
-                    },
+            const res = createRes({
+                issue: {
+                    createdAt: "2023-05-26T15:53:06Z",
+                    closedAt: "2023-05-26T16:00:37Z",
                 },
-            };
+            });
             axios.post.mockResolvedValueOnce(res);
             const result = await githubService.getTaskLeadTime(
                 expect.any(String),
@@ -58,7 +64,9 @@ describe("getTaskLeadTime", () => {
     });
 });
 
+// ---------------
 // async function getLeadTime(repo, owner, start, end)
+// ---------------
 describe("getLeadTime", () => {
     const res = {
         data: {
@@ -119,7 +127,9 @@ describe("getLeadTime", () => {
     });
 });
 
+// ---------------
 // async function getActiveTasksCount(repo, owner, columnName)
+// ---------------
 describe("getActiveTasksCount", () => {
     let columnName = "TODO";
     const res = {
@@ -175,7 +185,7 @@ describe("getActiveTasksCount", () => {
         axios.post.mockResolvedValueOnce(res);
     });
 
-    describe("when graphql call returns list of issues that are in column specified", () => {
+    describe("when graphql call returns list of issues for column specified", () => {
         it("should return their count", async () => {
             const result = await githubService.getActiveTasksCount(
                 expect.any(String),
@@ -186,7 +196,7 @@ describe("getActiveTasksCount", () => {
         });
     });
 
-    describe("when graphql call returns list of issues that aren't in column specified", () => {
+    describe("when graphql call returns no issues for column specified", () => {
         it("should return their count which is equal to 0", async () => {
             columnName = "In progress";
             const result = await githubService.getActiveTasksCount(
@@ -199,7 +209,9 @@ describe("getActiveTasksCount", () => {
     });
 });
 
+// ---------------
 // async function getCompletedTasksCount(repo, owner, start, end)
+// ---------------
 describe("getCompletedTasksCount", () => {
     let start = "2023-05-10";
     let end = "2023-05-31";
@@ -266,10 +278,180 @@ describe("getCompletedTasksCount", () => {
     });
 });
 
+// ---------------
 // async function getPRCount(owner, repo)
+// ---------------
+describe("getPRCount", () => {
+    describe.each([
+        [
+            "when graphql call returns a list of pull requests",
+            createRes({
+                pullRequests: {
+                    totalCount: 3,
+                },
+            }),
+            "should return their count",
+            "Number of pull requests: 3.",
+        ],
+    ])("%s", (name, res, testName, expected) => {
+        it(testName, async () => {
+            axios.post.mockResolvedValueOnce(res);
+            const result = await githubService.getPRCount(
+                expect.any(String),
+                expect.any(String)
+            );
+            expect(result).toBe(`Number of pull requests: 3.`);
+        });
+    });
+});
 
+// ---------------
 // async function getPRState(owner, repo, id)
+// ---------------
+describe("getPRState", () => {
+    let id = 11;
+    describe.each([
+        [
+            "when graphql call returns a pull request",
+            createRes({
+                pullRequest: { state: "MERGED" },
+            }),
+            "should return its state",
+            `State of pull request ${id}: MERGED.`,
+        ],
+        [
+            "when graphql call returns no pull request",
+            createRes({
+                pullRequest: null,
+            }),
+            "should return an error message",
+            `Pull request ${id} doesn't exists.`,
+        ],
+    ])("%s", (name, res, testName, expected) => {
+        it(testName, async () => {
+            axios.post.mockResolvedValueOnce(res);
+            const result = await githubService.getPRState(
+                expect.any(String),
+                expect.any(String),
+                id
+            );
+            expect(result).toBe(expected);
+        });
+    });
+});
+
+// ---------------
 // async function getPRMergeTime(owner, repo, id)
+// ---------------
+describe("getPRMergeTime", () => {
+    let id = 11;
+    describe.each([
+        [
+            "when graphql call returns a pull request",
+            createRes({
+                pullRequest: {
+                    mergedAt: "2023-05-30T20:17:55Z",
+                    createdAt: "2023-05-30T19:54:09Z",
+                },
+            }),
+            "should return its merge time",
+            `Merge time for pull request ${id}: 0 days, 0 hours, 23 minutes.`,
+        ],
+        [
+            "when graphql call returns no pull request",
+            createRes({
+                pullRequest: null,
+            }),
+            "should return an error message",
+            `Pull request ${id} doesn't exists.`,
+        ],
+    ])("%s", (name, res, testName, expected) => {
+        it(testName, async () => {
+            axios.post.mockResolvedValueOnce(res);
+            const result = await githubService.getPRMergeTime(
+                expect.any(String),
+                expect.any(String),
+                id
+            );
+            expect(result).toBe(expected);
+        });
+    });
+});
+
+// ---------------
 // async function getPRCommentsCount(owner, repo, id)
+// ---------------
+describe("getPRCommentsCount", () => {
+    let id = 11;
+    const pr = describe.each([
+        [
+            "when graphql call returns a pull request",
+            createRes({
+                pullRequest: {
+                    comments: {
+                        totalCount: 1,
+                    },
+                },
+            }),
+            "should return it's comments count",
+            `Number of comments for pull request ${id}: 1.`,
+        ],
+        [
+            "when graphql call returns no pull request",
+            createRes({ pullRequest: null }),
+            "should return an error message",
+            `Pull request ${id} doesn't exists.`,
+        ],
+    ])("%s", (name, res, testName, expected) => {
+        it(testName, async () => {
+            axios.post.mockResolvedValueOnce(res);
+            const result = await githubService.getPRCommentsCount(
+                expect.any(String),
+                expect.any(String),
+                id
+            );
+            expect(result).toBe(expected);
+        });
+    });
+});
+
+// ---------------
 // async function getPRReviewers(owner, repo, id)
-// async function getSnapshot()
+// ---------------
+describe("getPRReviewers", () => {
+    let id = 11;
+    describe.each([
+        [
+            "when graphql call returns a pull request",
+            createRes({
+                pullRequest: {
+                    reviewRequests: {
+                        nodes: [
+                            {
+                                requestedReviewer: { login: "msialercruz" },
+                            },
+                        ],
+                    },
+                },
+            }),
+            "should return it's reviewers",
+            `Reviewers for pull request ${id}: msialercruz.`,
+        ],
+        [
+            "when graphql call returns no pull request",
+            createRes({ pullRequest: null }),
+            "should return an error message",
+            `Pull request ${id} doesn't exists.`,
+        ],
+    ])("%s", (name, res, testName, expected) => {
+        it(testName, async () => {
+            axios.post.mockResolvedValueOnce(res);
+            const result = await githubService.getPRReviewers(
+                expect.any(String),
+                expect.any(String),
+                id
+            );
+            expect(result).toBe(expected);
+        });
+    });
+});
